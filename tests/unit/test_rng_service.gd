b675e7bd2a2b3303285f24918058faf17d943a69
Yeming <1816306453@qq.com>
2026-04-25 10:19:42 -0700

    fix: make Godot tests report failures

--- tests/unit/test_rng_service.gd
+++ tests/unit/test_rng_service.gd
@@ -2,16 +2,22 @@ extends RefCounted
 
 const RngService := preload("res://scripts/core/rng_service.gd")
 
-func test_same_seed_produces_same_sequence() -> void:
+func test_same_seed_produces_same_sequence() -> bool:
 	var a := RngService.new(12345)
 	var b := RngService.new(12345)
-	assert(a.next_int(1, 100) == b.next_int(1, 100))
-	assert(a.next_int(1, 100) == b.next_int(1, 100))
-	assert(a.pick(["a", "b", "c"]) == b.pick(["a", "b", "c"]))
+	var first_matches := a.next_int(1, 100) == b.next_int(1, 100)
+	var second_matches := a.next_int(1, 100) == b.next_int(1, 100)
+	var pick_matches: bool = a.pick(["a", "b", "c"]) == b.pick(["a", "b", "c"])
+	assert(first_matches)
+	assert(second_matches)
+	assert(pick_matches)
+	return first_matches and second_matches and pick_matches
 
-func test_fork_is_deterministic_by_label() -> void:
+func test_fork_is_deterministic_by_label() -> bool:
 	var root_a := RngService.new(777)
 	var root_b := RngService.new(777)
 	var map_a = root_a.fork("map")
 	var map_b = root_b.fork("map")
-	assert(map_a.next_int(0, 999) == map_b.next_int(0, 999))
+	var fork_matches: bool = map_a.next_int(0, 999) == map_b.next_int(0, 999)
+	assert(fork_matches)
+	return fork_matches
